ef0551c20e0c3b86687fe8a835c5aa62a21b92df
jc3213 <jc3213@users.noreply.github.com>
2015-01-23 21:27:00 +0900

    Update bootstrap.js
    Minor tweak

--- Anti-ads-Player/Source/bootstrap.js
+++ Anti-ads-Player/Source/bootstrap.js
@@ -2,116 +2,117 @@ const {classes: Cc, interfaces: Ci, results: Cr, utils: Cu} = Components;
 Cu.import('resource://gre/modules/NetUtil.jsm');
 
 var aURI = 'chrome://antiadsplayer/content';
+var PLAYERS = {
+/**  -------------------------------------------------------------------------------------------------------  */
+  'youku_loader': {
+    'object': aURI + '/loader.swf',
+    'target': /http:\/\/static\.youku\.com\/.*\/v\/swf\/loaders?\.swf/i
+  },
+  'youku_player': {
+    'object': aURI + '/player.swf',
+    'target': /http:\/\/static\.youku\.com\/.*\/v\/swf\/q?player.*\.swf/i
+  },
+/**  -------------------------------------------------------------------------------------------------------  */
+  'tudou_portal': {
+    'object': aURI + '/tudou.swf',
+    'target': /http:\/\/js\.tudouui\.com\/bin\/lingtong\/PortalPlayer.*\.swf/i
+  },
+  'tudou_olc': {
+    'object': 'http://js.tudouui.com/bin/player2/olc.swf',
+    'target': /http:\/\/js\.tudouui\.com\/bin\/player2\/olc.+\.swf/i
+  },
+  'tudou_social': {
+    'object': aURI + '/sp.swf',
+    'target': /http:\/\/js\.tudouui\.com\/bin\/lingtong\/SocialPlayer.*\.swf/i
+  },
+/**  -------------------------------------------------------------------------------------------------------  */
+  'iqiyi5': {
+    'object': aURI + '/iqiyi5.swf',
+    'target': /http:\/\/www\.iqiyi\.com\/common\/flashplayer\/\d+\/MainPlayer.*\.swf/i
+  },
+  'iqiyi_out': {
+    'object': aURI + '/iqiyi_out.swf',
+    'target': /https?:\/\/www\.iqiyi\.com\/(common\/flash)?player\/\d+\/(Share)?Player.*\.swf/i
+  },
+/**  -------------------------------------------------------------------------------------------------------  */
+  'pps': {
+    'object': aURI + '/iqiyi.swf',
+    'target': /http:\/\/www\.iqiyi\.com\/common\/flashplayer\/\d+\/PPSMainPlayer.*\.swf/i
+  },
+  'pps_out': {
+    'object': aURI + '/pps.swf',
+    'target': /http:\/\/www\.iqiyi\.com\/player\/cupid\/common\/pps_flvplay_s\.swf/i
+  },
+/**  -------------------------------------------------------------------------------------------------------  */
+  'letv': {
+    'object': aURI + '/letv.swf',
+    'target': /http:\/\/.*\.letv(cdn)?\.com\/.*(new)?player\/((SDK)?Letv|swf)Player\.swf/i
+  },
+  'letv_skin': {
+    'object': 'http://player.letvcdn.com/p/201407/24/15/newplayer/1/SSLetvPlayer.swf',
+    'target': /http:\/\/player\.letvcdn\.com\/p\/((?!15)\d+\/){3}newplayer\/1\/S?SLetvPlayer\.swf/i
+  },
+/**  -------------------------------------------------------------------------------------------------------  */
+  'sohu': {
+    'object': aURI + '/sohu_live.swf',
+    'target': /http:\/\/(tv\.sohu\.com\/upload\/swf\/(p2p\/)?\d+|(\d+\.){3}\d+\/webplayer)\/Main\.swf/i
+  },
+/**  -------------------------------------------------------------------------------------------------------  */
+  'pptv': {
+    'object': aURI + '/pptv.in.Ikan.swf',
+    'target': /http:\/\/player.pplive.cn\/ikan\/.*\/player4player2\.swf/i
+  },
+  'pptv_live': {
+    'object': aURI + '/pptv.in.Live.swf',
+    'target': /http:\/\/player.pplive.cn\/live\/.*\/player4live2\.swf/i
+  },
+/**  -------------------------------------------------------------------------------------------------------  */
+  '17173': {
+    'object': aURI + '/17173.in.Vod.swf',
+    'target': /http:\/\/f\.v\.17173cdn\.com\/\d+\/flash\/Player_file\.swf/i
+  },
+  '17173_out': {
+    'object': aURI + '/17173.out.Vod.swf',
+    'target': /http:\/\/f\.v\.17173cdn\.com\/(\d+\/)?flash\/Player_file_(custom)?out\.swf/i
+  },
+  '17173_live': {
+    'object': aURI + '/17173.in.Live.swf',
+    'target': /http:\/\/f\.v\.17173cdn\.com\/\d+\/flash\/Player_stream(_firstpage)?\.swf/i
+  },
+  '17173_live_out': {
+    'object': aURI + '/17173.out.Live.swf',
+    'target': /http:\/\/f\.v\.17173cdn\.com\/\d+\/flash\/Player_stream_(custom)?Out\.swf/i
+  },
+/**  -------------------------------------------------------------------------------------------------------  */
+  'ku6': {
+    'object': aURI + '/ku6_in_player.swf',
+    'target': /http:\/\/player\.ku6cdn\.com\/default\/(\w+\/){2}\d+\/player\.swf/i
+  },
+  'ku6_out': {
+    'object': aURI + '/ku6_out_player.swf',
+    'target': /http:\/\/player\.ku6cdn\.com\/default\/out\/\d+\/player\.swf/i
+  },
+/**  -------------------------------------------------------------------------------------------------------  */
+  'baidu': {
+    'object': aURI + '/baidu.call.swf',
+    'target': /http:\/\/list\.video\.baidu\.com\/swf\/advPlayer\.swf/i
+  },
+};
+
+var REFERERS = {
+/**  -------------------------------------------------------------------------------------------------------  */
+  'youku': {
+    'host': 'http://www.youku.com/',
+    'target': /http:\/\/.*\.youku\.com/i
+  },
+/**  -------------------------------------------------------------------------------------------------------  */
+  'iqiyi': {
+    'host': 'http://www.iqiyi.com/',
+    'target': /http:\/\/.*\.qiyi\.com/i
+  },
+};
 
 var aCommon = {
-  PLAYERS: {
-/**  -------------------------------------------------------------------------------------------------------  */
-    'youku_loader': {
-      'object': aURI + '/loader.swf',
-      'target': /http:\/\/static\.youku\.com\/.*\/v\/swf\/loaders?\.swf/i
-    },
-    'youku_player': {
-      'object': aURI + '/player.swf',
-      'target': /http:\/\/static\.youku\.com\/.*\/v\/swf\/q?player.*\.swf/i
-    },
-/**  -------------------------------------------------------------------------------------------------------  */
-    'tudou_portal': {
-      'object': aURI + '/tudou.swf',
-      'target': /http:\/\/js\.tudouui\.com\/bin\/lingtong\/PortalPlayer.*\.swf/i
-    },
-    'tudou_olc': {
-      'object': 'http://js.tudouui.com/bin/player2/olc.swf',
-      'target': /http:\/\/js\.tudouui\.com\/bin\/player2\/olc.+\.swf/i
-    },
-    'tudou_social': {
-      'object': aURI + '/sp.swf',
-      'target': /http:\/\/js\.tudouui\.com\/bin\/lingtong\/SocialPlayer.*\.swf/i
-    },
-/**  -------------------------------------------------------------------------------------------------------  */
-    'iqiyi5': {
-      'object': aURI + '/iqiyi5.swf',
-      'target': /http:\/\/www\.iqiyi\.com\/common\/flashplayer\/\d+\/MainPlayer.*\.swf/i
-},
-    'iqiyi_out': {
-      'object': aURI + '/iqiyi_out.swf',
-      'target': /https?:\/\/www\.iqiyi\.com\/(common\/flash)?player\/\d+\/(Share)?Player.*\.swf/i
-},
-/**  -------------------------------------------------------------------------------------------------------  */
-    'pps': {
-      'object': aURI + '/iqiyi.swf',
-      'target': /http:\/\/www\.iqiyi\.com\/common\/flashplayer\/\d+\/PPSMainPlayer.*\.swf/i
-    },
-    'pps_out': {
-      'object': aURI + '/pps.swf',
-      'target': /http:\/\/www\.iqiyi\.com\/player\/cupid\/common\/pps_flvplay_s\.swf/i
-    },
-/**  -------------------------------------------------------------------------------------------------------  */
-    'letv': {
-      'object': aURI + '/letv.swf',
-      'target': /http:\/\/.*\.letv(cdn)?\.com\/.*(new)?player\/((SDK)?Letv|swf)Player\.swf/i
-    },
-    'letv_skin': {
-      'object': 'http://player.letvcdn.com/p/201407/24/15/newplayer/1/SSLetvPlayer.swf',
-      'target': /http:\/\/player\.letvcdn\.com\/p\/((?!15)\d+\/){3}newplayer\/1\/S?SLetvPlayer\.swf/i
-    },
-/**  -------------------------------------------------------------------------------------------------------  */
-    'sohu': {
-      'object': aURI + '/sohu_live.swf',
-      'target': /http:\/\/(tv\.sohu\.com\/upload\/swf\/(p2p\/)?\d+|(\d+\.){3}\d+\/webplayer)\/Main\.swf/i
-    },
-/**  -------------------------------------------------------------------------------------------------------  */
-    'pptv': {
-      'object': aURI + '/pptv.in.Ikan.swf',
-      'target': /http:\/\/player.pplive.cn\/ikan\/.*\/player4player2\.swf/i
-    },
-    'pptv_live': {
-      'object': aURI + '/pptv.in.Live.swf',
-      'target': /http:\/\/player.pplive.cn\/live\/.*\/player4live2\.swf/i
-    },
-/**  -------------------------------------------------------------------------------------------------------  */
-    '17173': {
-      'object': aURI + '/17173.in.Vod.swf',
-      'target': /http:\/\/f\.v\.17173cdn\.com\/\d+\/flash\/Player_file\.swf/i
-    },
-    '17173_out': {
-      'object': aURI + '/17173.out.Vod.swf',
-      'target': /http:\/\/f\.v\.17173cdn\.com\/(\d+\/)?flash\/Player_file_(custom)?out\.swf/i
-    },
-    '17173_live': {
-      'object': aURI + '/17173.in.Live.swf',
-      'target': /http:\/\/f\.v\.17173cdn\.com\/\d+\/flash\/Player_stream(_firstpage)?\.swf/i
-    },
-    '17173_live_out': {
-      'object': aURI + '/17173.out.Live.swf',
-      'target': /http:\/\/f\.v\.17173cdn\.com\/\d+\/flash\/Player_stream_(custom)?Out\.swf/i
-    },
-/**  -------------------------------------------------------------------------------------------------------  */
-    'ku6': {
-      'object': aURI + '/ku6_in_player.swf',
-      'target': /http:\/\/player\.ku6cdn\.com\/default\/(\w+\/){2}\d+\/player\.swf/i
-    },
-    'ku6_out': {
-      'object': aURI + '/ku6_out_player.swf',
-      'target': /http:\/\/player\.ku6cdn\.com\/default\/out\/\d+\/player\.swf/i
-    },
-/**  -------------------------------------------------------------------------------------------------------  */
-    'baidu': {
-      'object': aURI + '/baidu.call.swf',
-      'target': /http:\/\/list\.video\.baidu\.com\/swf\/advPlayer\.swf/i
-    },
-  },
-  DOMAINS: {
-/**  -------------------------------------------------------------------------------------------------------  */
-    'youku': {
-      'host': 'http://www.youku.com/',
-      'target': /http:\/\/.*\.youku\.com/i
-    },
-/**  -------------------------------------------------------------------------------------------------------  */
-    'iqiyi': {
-      'host': 'http://www.iqiyi.com/',
-      'target': /http:\/\/.*\.qiyi\.com/i
-    },
-  },
   oService: Cc['@mozilla.org/observer-service;1'].getService(Ci.nsIObserverService),
   getObject: function (rule, callback) {
     NetUtil.asyncFetch(rule['object'], function (inputStream, status) {
@@ -147,8 +148,8 @@ var aCommon = {
   observe: function (aSubject, aTopic, aData) {
     if (aTopic == "http-on-modify-request") {
     var httpReferer = aSubject.QueryInterface(Ci.nsIHttpChannel);
-    for (var i in this.DOMAINS) {
-      var domain = this.DOMAINS[i];
+    for (var i in REFERERS) {
+      var domain = REFERERS[i];
         try {
         var URL = httpReferer.originalURI.spec;
           if (domain['target'].test(URL)) {
@@ -181,8 +182,8 @@ var aCommon = {
     httpChannel.visitResponseHeaders(aVisitor);
     if (!aVisitor.isFlash()) return;
 
-    for (var i in this.PLAYERS) {
-      var rule = this.PLAYERS[i];
+    for (var i in PLAYERS) {
+      var rule = PLAYERS[i];
       if (rule['target'].test(httpChannel.URI.spec)) {
         var fn = this, args = Array.prototype.slice.call(arguments);
         if (typeof rule['preHandle'] === 'function')
@@ -209,7 +210,7 @@ var aCommon = {
     return Cr.NS_ERROR_NO_INTERFACE;
   },
   aResolver: function () {
-    var rule = this.PLAYERS['iqiyi'];
+    var rule = PLAYERS['iqiyi'];
     if (!rule) return;
     rule['preHandle'] = function (aSubject) {
       var wnd = this.getWindowForRequest(aSubject);
