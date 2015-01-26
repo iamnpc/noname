8b83981eaac97238520bcb7fd72113a389d4b16d
jc3213 <jc3213@users.noreply.github.com>
2015-01-26 15:45:37 +0900

    Default prefs.js from Adblock Edge 2.1.7

---
+++ Adblock-Edge/defaults/prefs.js
@@ -0,0 +1,27 @@
+// For description of these values see http://adblockplus.org/en/preferences
+
+pref("extensions.adblockedge.currentVersion", "0.0");
+pref("extensions.adblockedge.enabled", true);
+pref("extensions.adblockedge.frameobjects", true);
+pref("extensions.adblockedge.fastcollapse", false);
+pref("extensions.adblockedge.showinstatusbar", false);
+pref("extensions.adblockedge.detachsidebar", false);
+pref("extensions.adblockedge.defaulttoolbaraction", 0);
+pref("extensions.adblockedge.defaultstatusbaraction", 0);
+pref("extensions.adblockedge.sidebar_key", "Accel Shift U");
+pref("extensions.adblockedge.filters_key", "Accel Shift E, Accel Shift F, Accel Shift O");
+pref("extensions.adblockedge.enable_key", "");
+pref("extensions.adblockedge.flash_scrolltoitem", true);
+pref("extensions.adblockedge.previewimages", true);
+pref("extensions.adblockedge.data_directory", "adblockedge");
+pref("extensions.adblockedge.patternsbackups", 5);
+pref("extensions.adblockedge.patternsbackupinterval", 24);
+pref("extensions.adblockedge.whitelistschemes", "about chrome file irc moz-safe-about news resource snews x-jsd addbook cid imap mailbox nntp pop data javascript moz-icon");
+pref("extensions.adblockedge.subscriptions_autoupdate", true);
+pref("extensions.adblockedge.subscriptions_listurl", "https://rawgit.com/adstomper/subs/master/subscriptions2.xml");
+pref("extensions.adblockedge.documentation_link", "http://adstomper.bitbucket.org/");
+pref("extensions.adblockedge.savestats", false);
+pref("extensions.adblockedge.composer_default", 2);
+pref("extensions.adblockedge.clearStatsOnHistoryPurge", true);
+pref("extensions.adblockedge.blockableItemsSize", {width: 200, height: 200});
+pref("extensions.adblockedge.disable_startup_delay", true);
